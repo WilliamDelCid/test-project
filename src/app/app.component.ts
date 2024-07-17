47223a3e8bdcfe9ebbbfbff4bae87645128c8a69
williamdelcid <dm18019@ues.edu.sv>
2024-07-16 22:29:38 -0600

    Change

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  title = 'test-project';
+
+  ngOnInit(): void {}
+
+  constructor() {
+    console.log('AppComponent constructor');
+  }
+
+}
